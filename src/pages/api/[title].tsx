1eb9d98e8bac42426a94e12330b6e41391a607b2
object1037 <contact@object1037.dev>
2022-10-07 21:37:00 +0900

    fix

--- src/pages/api/[title].tsx
+++ src/pages/api/[title].tsx
@@ -7,14 +7,14 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
   if (!title || typeof title !== 'string') {
     return res.status(400).end('Bad Request')
   }
-  registerFont(path.resolve('./public/ZenMaruGothic.ttf'), {
+  registerFont(path.resolve('./assets/ZenMaruGothic.ttf'), {
     family: 'ZenMaruGothic',
   })
   const canvas = createCanvas(1200, 600)
   const ctx = canvas.getContext('2d')
   ctx.font = '64px "ZenMaruGothic"'
 
-  loadImage(path.resolve('./public/bg.png')).then((image) => {
+  loadImage(path.resolve('./assets/bg.png')).then((image) => {
     ctx.drawImage(image, 0, 0, 1200, 600)
     ctx.fillText(title, 100, 150)
     res.status(200).end(canvas.toBuffer())
