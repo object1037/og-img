042d995cf55e74704fe24f81f511cfdaa40f16f4
object1037 <contact@object1037.dev>
2022-10-07 20:57:43 +0900

    init

---
+++ src/pages/api/[title].tsx
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { createCanvas, loadImage, registerFont } from 'canvas'
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { title } = req.query
+  if (!title || typeof title !== 'string') {
+    return res.status(400).end('Bad Request')
+  }
+  registerFont('./public/ZenMaruGothic.ttf', { family: 'ZenMaruGothic' })
+  const canvas = createCanvas(1200, 600)
+  const ctx = canvas.getContext('2d')
+  ctx.font = '64px "ZenMaruGothic"'
+
+  loadImage('./public/bg.png').then((image) => {
+    ctx.drawImage(image, 0, 0, 1200, 600)
+    ctx.fillText(title, 100, 150)
+    res.status(200).end(canvas.toBuffer())
+  })
+}
+
+export default handler
