95c2f86b8436db30be3e75e77b061b345fdaa559
object1037 <contact@object1037.dev>
2022-10-08 01:02:52 +0900

    add readme and index content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
+import Image from 'next/future/image'
+
 const Home = () => {
-  return <div>hoge</div>
+  return (
+    <main
+      style={{
+        padding: '4rem',
+        maxWidth: '56rem',
+      }}
+    >
+      <h1 style={{ marginBottom: '3rem' }}>og-image as a service.</h1>
+      <a
+        href="/テストタイトル"
+        style={{
+          filter: 'drop-shadow(0 25px 25px rgb(0 0 0 / 0.15))',
+        }}
+      >
+        <Image
+          src="/テストタイトル"
+          alt="「テストタイトル」のOGP画像"
+          width={1200}
+          height={600}
+          style={{ maxWidth: '100%', height: 'auto', borderRadius: '0.75rem' }}
+        />
+      </a>
+    </main>
+  )
 }
 
 export default Home
