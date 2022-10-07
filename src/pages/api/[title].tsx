90c61bcc6d818b912b1d1a112fa50b6dc67d2a1f
object1037 <contact@object1037.dev>
2022-10-07 22:30:37 +0900

    switch to @napi-rs/canvas

--- src/pages/api/[title].tsx
+++ src/pages/api/[title].tsx
@@ -1,24 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { createCanvas, loadImage, registerFont } from 'canvas'
+import { createCanvas, GlobalFonts, Image } from '@napi-rs/canvas'
 import path from 'path'
+import { promises } from 'fs'
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { title } = req.query
   if (!title || typeof title !== 'string') {
     return res.status(400).end('Bad Request')
   }
-  registerFont(path.resolve('./assets/ZenMaruGothic.ttf'), {
-    family: 'ZenMaruGothic',
-  })
+
+  GlobalFonts.registerFromPath(
+    path.resolve('./assets/ZenMaruGothic.woff2'),
+    'ZenMaruGothic'
+  )
+
+  const imageBuffer = await promises.readFile(path.resolve('./assets/bg.png'))
+  const image = new Image()
+  image.src = imageBuffer
+
   const canvas = createCanvas(1200, 600)
   const ctx = canvas.getContext('2d')
-  ctx.font = '64px "ZenMaruGothic"'
 
-  loadImage(path.resolve('./assets/bg.png')).then((image) => {
-    ctx.drawImage(image, 0, 0, 1200, 600)
-    ctx.fillText(title, 100, 150)
-    res.status(200).end(canvas.toBuffer())
-  })
+  ctx.font = '64px ZenMaruGothic'
+  ctx.drawImage(image, 0, 0, 1200, 600)
+  ctx.fillText(title, 100, 150)
+  return res.status(200).end(canvas.toBuffer('image/png'))
 }
 
 export default handler
